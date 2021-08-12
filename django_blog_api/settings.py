8c8b801d0f580aa1c693891c02a5cb2f06d8383e
sharhan-alhassan <sharhanalhassan@gmail.com>
2021-08-12 19:04:35 +0000

    created superuser and made initial migrations

--- django_blog_api/settings.py
+++ django_blog_api/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # locat apps
+    'api.apps.ApiConfig',
+
+    # Installed apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +60,7 @@ ROOT_URLCONF = 'django_blog_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
